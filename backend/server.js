db79c49292468aa9e9745b488f10e04925f1c8ac
samincgs <samincgs@gmail.com>
2023-05-10 20:22:33 -0400

    created a routes folder and created a workotu file with multiple routes to the server

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,21 @@
 //all imports & dependencies
 const express = require('express');
+const workoutRoutes = require('./routes/workouts');
 require('dotenv').config();
 
 //express app
 const app = express();
 
 //middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
 //routes
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the app' });
-});
+app.use('/api/workouts', workoutRoutes);
 
 //listening for requests
 app.listen(process.env.PORT, () => {
