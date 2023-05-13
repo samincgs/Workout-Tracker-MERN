e2c057fcb512a0cfbfd11d838a3d255885ee039c
samincgs <samincgs@gmail.com>
2023-05-13 02:24:56 -0400

    Users can now delete workouts that they have previously added

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,4 +1,19 @@
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
+
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+
+  const handleClick = async () => {
+    const response = await fetch(`/api/workouts/${workout._id}`, {
+      method: 'DELETE',
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: 'DELETE_WORKOUT', payload: json });
+    }
+  };
+
   return (
     <div className='workout-details'>
       <h3>{workout.title}</h3>
@@ -11,6 +26,7 @@ const WorkoutDetails = ({ workout }) => {
       <p>
         <strong>Created:</strong> {workout.createdAt}
       </p>
+      <span onClick={handleClick}>delete</span>
     </div>
   );
 };
